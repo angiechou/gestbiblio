60c671a39579aa2d6d7549bc9f479aeef51a261f
Angelo Adanhounme <pro.angeloadanhounme@gmail.com>
2026-06-11 20:18:02 +0100

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,21 +4,22 @@ $role = $_SESSION['user']['role'];
 ?>
 
 <div class="sidebar">
-    <div class="sidebar-logo">
-        GESTBIBLIO <br> <small style="font-size: 12px; font-weight: normal;">GESTIONNAIRE</small>
-    </div>
-    
-    <ul class="nav-links">
-        <?php if ($role === 'admin'): ?>
-            <!-- Menu Administrateur[cite: 3] -->
-            <li><a href="<?php echo $base_path; ?>admin/dashboard.php">Dashboard (Stats)</a></li>
-            <li><a href="<?php echo $base_path; ?>admin/ajouter.php">Ajouter un livre</a></li>
-            <li><a href="<?php echo $base_path; ?>admin/catalogue.php">Liste & Retrait</a></li>
-            <li><a href="<?php echo $base_path; ?>admin/demandes.php">Valider Emprunts</a></li>
-        <?php else: ?>
-            <!-- Menu Étudiant (User)[cite: 3] -->
-            <li><a href="<?php echo $base_path; ?>user/dashboard.php">Dashboard (Mes Emprunts)</a></li>
-            <li><a href="<?php echo $base_path; ?>user/catalogue.php">Catalogue des Livres</a></li>
-        <?php endif; ?>
-    </ul>
+    <div class="sidebar-title">Navigation</div>
+
+    <!-- LIENS RÉSERVÉS UNIQUEMENT À L'ADMINISTRATEUR -->
+    <?php if ($role === 'admin'): ?>       
+        <a href="../admin/dashboard.php">Tableau de bord</a>
+        <a href="../admin/ajouter.php">Ajouter un Livre</a>
+        <a href="../admin/catalogue.php">Catalogue</a>
+        <a href="../admin/demandes.php">Demandes</a>
+
+    <!-- LIENS RÉSERVÉS UNIQUEMENT AUX ÉTUDIANTS -->
+    <?php else: ?>
+        <a href="../user/dashboard.php">Tableau de bord</a>
+        <a href="../user/catalogue.php">Catalogue</a>
+    <?php endif; ?>
+
+    <div class="menu-divider">Mon Compte</div>
+    <a href="../profil.php">Modifier mon Profil</a>
+    <a href="../index.php" style="color: #e74c3c;">Déconnexion</a>
 </div>
