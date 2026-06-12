d0cf7c369c1ccfbd76467b72cade2dffb0dd25fe
Angelo Adanhounme <pro.angeloadanhounme@gmail.com>
2026-06-12 10:49:00 +0100

    Deuxieme modification du script SQL

--- database/scripts.sql
+++ database/scripts.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE bibliotheque;
 
+USE biblotheque;
+
 CREATE TABLE `utilisateur` (
   `id_utilisateur` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `username` varchar(100) NOT NULL,
@@ -8,7 +10,7 @@ CREATE TABLE `utilisateur` (
   `photo` varchar(255) DEFAULT 'default.png',
   `role` enum('user','admin') DEFAULT 'user',
   `date_creation` datetime DEFAULT curdate()
-)
+);
 
 CREATE TABLE `livre` (
   `id_livre` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -16,7 +18,7 @@ CREATE TABLE `livre` (
   `auteur` varchar(255) NOT NULL,
   `stock_total` int(11) NOT NULL,
   `stock_dispo` int(11) NOT NULL
-)
+);
 
 CREATE TABLE `emprunter` (
   `id_emprunt` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -27,8 +29,8 @@ CREATE TABLE `emprunter` (
   `statut` enum('en_attente','confirme','rendu') DEFAULT 'en_attente',
   FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur`(`id_utilisateur`) ON DELETE CASCADE,
   FOREIGN KEY (`id_livre`) REFERENCES `livre`(`id_livre`) ON DELETE CASCADE
-)
+);
 
 -- Un compte admin est crée directement pour accès unique
 INSERT INTO utilisateur (username, email, password, photo, role, date_creation ) VALUES
-( 'admin', 'admin@gestbiblio.com', 'admin123', 'default.png', 'admin', NOW());
+( 'admin', 'admin@gestbiblio.com', '$2y$10$KK/JPcuigKbM6U7.N5NMTezlyYMa5qkbBquYBR5rNDgRRe/130pye', 'default.png', 'admin', NOW());
