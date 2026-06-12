ac80719f0e13f1dd1105098752914d039e61ad06
Angelo Adanhounme <pro.angeloadanhounme@gmail.com>
2026-06-12 13:53:51 +0100

    Update logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -2,6 +2,6 @@
     session_start();
     session_unset(); // Supprime toutes les variables de session
     session_destroy(); // Détruit la session
-    header("Location: ../includes/index.php"); // Redirige vers la page de connexion
+    header("Location:../index.php"); // Redirige vers la page de connexion
     exit();
 ?>
